95f2e1ed37a3a664438a142e5196f232d9285f96
wngh1685 <wngh1685@gmail.com>
2024-08-21 23:01:26 +0900

    사진 출력 후 ai 모델을 이용하여 표정 인식 출력 확인

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, upload_image
+from .views import home, upload_image, recognition
 
 urlpatterns = [
     path('', home, name='home'),
     path('upload/', upload_image, name='upload'),
+    path('recognition/', recognition, name='recognition'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,29 @@
+import tensorflow as tf
+import numpy as np
+import os
 from django.shortcuts import render
 from django.conf import settings
-import os
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+
+# 1. 모델 로드
+model_path = "models-for-facial-expression-recognition-tensorflow2-default-v1/best_simple_model"  # .pb 파일이 있는 디렉토리 경로
+model = tf.saved_model.load(model_path)
+
+# 2. 서명(Signature) 함수 가져오기
+# 보통 'serving_default'라는 서명으로 모델이 저장됩니다.
+infer = model.signatures['serving_default']
+
+
+# 3. 입력 데이터 준비
+def preprocess_image(image_path):
+    img = tf.keras.preprocessing.image.load_img(image_path, color_mode='grayscale', target_size=(48, 48))  # 흑백 이미지로 로드
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)  # 배치 차원 추가 (1, 48, 48, 1)
+    img_array = img_array.astype(np.float32)  # dtype을 float32로 변환
+    img_array = img_array / 255.0  # 정규화
+    return img_array
 
 
 def home(request):
@@ -26,5 +49,42 @@ def upload_image(request):
             'image_url': settings.MEDIA_URL + uploaded_file.name
         }
         return render(request, 'upload.html', context)
+
+    return render(request, 'upload.html')
+
+
+# 4. 업로드 이미지 처리 및 모델 예측
+def recognition(request):
+    if request.method == 'POST':
+        # 업로드된 이미지 파일 경로 가져오기
+        image_url = request.POST.get('image_name')
+
+        # 첫 번째 '/' 기호만 제거하기
+        if image_url.startswith('/'):
+            image_name = image_url[1:]  # '/' 기호를 제거하여 새로운 문자열 생성
+        else:
+            image_name = image_url
+
+        image_path = os.path.join(settings.MEDIA_ROOT, image_name)
+
+        # 입력 데이터 준비
+        input_data = preprocess_image(image_path)
+        input_tensor = tf.convert_to_tensor(input_data)
+
+        # 모델 예측
+        predictions = infer(input_1=input_tensor)
+
+        # 예측 결과 해석
+        emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+        predicted_label = np.argmax(predictions['dense_1'].numpy(), axis=-1)  # 확률이 가장 높은 인덱스 선택
+        decoded_label = emotion_labels[predicted_label[0]]  # 해당 인덱스의 감정 레이블
+
+        # 결과를 템플릿으로 전달
+        context = {
+            'image_url': settings.MEDIA_URL + image_name,
+            'predicted_emotion': decoded_label,
+        }
+        # return render(request, 'upload.html', context)
+        return HttpResponseRedirect(reverse('upload'))
 
     return render(request, 'upload.html')
